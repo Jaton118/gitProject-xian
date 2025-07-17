ae4fd933b7042e7c445987c6fb629de2a0ad6ba7
Jaton118 <tedgeide@gmail.com>
2025-07-17 06:28:50 +0000

    this is for task2

---
+++ MyFirstApp.java
@@ -0,0 +1,8 @@
+public class MyFirstApp {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+                System.out.println("Ahhhhhhhhhhhhhhhh");
+				lollol
+	}// this is for task2
+}//this is for task 3
